e6d44fa35ed83af2a6c067f806823315c2334344
Boris Kaiser <mail@boriskaiser.com>
2015-04-10 20:44:54 +0200

    Cleanup

--- gulpfile.js/tasks/styles.js
+++ gulpfile.js/tasks/styles.js
@@ -2,26 +2,33 @@
 
 var _stylesFolder = 'styles';
 var _stylesSrcPath = _paths.src + '/' + _stylesFolder;
+var _autoprefixerConfig = {
+	browsers: ['last 3 versions'],
+	cascade: false
+};
 
 gulp.task('_sass', function () {
 	return gulp.src(_stylesSrcPath + '/styles.scss')
+
 		.pipe($.plumber({
 			errorHandler: $.notify.onError({
 				title: "Styles task failed",
 				message: "<%= error.message %>"
 			})
 		}))
+
 		.pipe($.if(_paths.current === _paths.tmp, $.sourcemaps.init()))
 		.pipe($.sass())
 		.pipe($.if(_paths.current === _paths.tmp, $.sourcemaps.write()))
-		.pipe($.sourcemaps.init({ loadMaps:true }))
-		.pipe($.autoprefixer({
-			browsers: ['last 3 versions'],
-			cascade: false
-		}))
+
+		.pipe($.sourcemaps.init({loadMaps: true}))
+		.pipe($.autoprefixer(_autoprefixerConfig))
 		.pipe($.sourcemaps.write())
+
 		.pipe($.if(_paths.current === _paths.dist, $.csso()))
+
 		.pipe(gulp.dest(_paths.current + '/' + _stylesFolder))
+
 		.pipe(reload({stream: true}));
 });
 
