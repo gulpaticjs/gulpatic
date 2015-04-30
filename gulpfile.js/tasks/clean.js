67c72813f7b03fbb9810bb7b4e1dcc7b21889087
Boris Kaiser <mail@boriskaiser.com>
2015-04-30 11:25:32 +0200

    Refactore clean tasks

--- gulpfile.js/tasks/clean.js
+++ gulpfile.js/tasks/clean.js
@@ -2,10 +2,27 @@
 
 var del = require('del');
 
-// Clean all paths
-gulp.task('clean', function () {
-	del.sync([
-		_paths.tmp + '/*', '!' + _paths.tmp + '/.gitkeep',
-		_paths.dist + '/*', '!' + _paths.dist + '/.gitkeep'
-	]);
+
+// Clean temp folder
+gulp.task('clean:tmp', function () {
+	del.sync([_paths.tmp + '/*', '!' + _paths.tmp + '/.gitkeep']);
+});
+
+// Clean dist folder
+gulp.task('clean:dist', function () {
+	del.sync([_paths.dist + '/*', '!' + _paths.tmp + '/.gitkeep']);
+});
+
+// Clean builds folder
+gulp.task('clean:builds', function () {
+	del.sync([_paths.builds + '/*', '!' + _paths.tmp + '/.gitkeep']);
+});
+
+
+// Clean temp and dist folder
+gulp.task('clean', function (callback) {
+	runSequence(
+		['clean:tmp', 'clean:dist'],
+		callback
+	);
 });
