b155e8a54c6b57c4c6a6ca45d67427546506e405
Boris Kaiser <mail@boriskaiser.com>
2016-04-21 22:46:18 +0200

    Update home

--- app/src/scripts/intro-animation.js
+++ app/src/scripts/intro-animation.js
@@ -1,14 +1,18 @@
 import dynamics from 'dynamics.js';
 
 const body = document.querySelector('body');
-const logo = document.querySelector('.svg--logo');
-const p = document.querySelector('p');
+const logo = document.querySelector('[data-js-logo]');
+const title = document.querySelector('[data-js-title]');
+const twitterLink = document.querySelector('[data-js-twitter]');
+const githubLogo = document.querySelector('[data-js-github-logo] svg');
 
 dynamics.css(body, { scale: 1.1 });
 dynamics.css(logo, { opacity: 0, translateY: 15 });
-dynamics.css(p, { opacity: 0, translateY: -15 });
+dynamics.css(title, { opacity: 0, translateY: -15 });
+dynamics.css(githubLogo, { opacity: 0, translateY: -15 });
+dynamics.css(twitterLink, { opacity: 0 });
 
-function animateElements() {
+function animateContent() {
   // Logo
   dynamics.animate(logo, {
     opacity: 1,
@@ -19,12 +23,31 @@ function animateElements() {
   });
 
   // Paragraph
-  dynamics.animate(p, {
+  dynamics.animate(title, {
     opacity: 1,
     translateY: 0,
   }, {
     delay: 50,
     type: dynamics.spring,
+  });
+
+  // Github logo
+  dynamics.animate(githubLogo, {
+    opacity: 1,
+    translateY: 0,
+  }, {
+    delay: 750,
+    type: dynamics.spring,
+    frequency: 140,
+    friction: 220,
+  });
+
+  // Twitter link
+  dynamics.animate(twitterLink, {
+    opacity: 1,
+  }, {
+    delay: 750,
+    type: dynamics.easeIn,
   });
 }
 
@@ -36,7 +59,7 @@ function animateBody() {
   }, {
     duration: 1500,
     type: dynamics.easeInOut,
-    complete: animateElements,
+    complete: animateContent,
   });
 }
 
